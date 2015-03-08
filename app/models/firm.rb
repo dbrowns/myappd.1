ebc67878fa31922b0c560e35bd121901dc3e6c31
Prelang Builder <builder@prelang.com>
2015-03-08 16:56:10 +0000

    Defined validation for attribute 'website' on model 'Firm'

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -3,4 +3,5 @@ class Firm < ActiveRecord::Base
   has_many :posts
   validates_formatting_of :email, using: :email
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :website, using: :url
 end
