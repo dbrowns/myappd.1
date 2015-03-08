f46b3544790b215ea2dcc5523aa3b835328104af
Prelang Builder <builder@prelang.com>
2015-03-08 16:56:34 +0000

    Made model 'Firm' votable by adding acts_as_votable

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -4,4 +4,5 @@ class Firm < ActiveRecord::Base
   validates_formatting_of :email, using: :email
   validates_formatting_of :phone, using: :us_phone
   validates_formatting_of :website, using: :url
+  acts_as_votable 
 end
